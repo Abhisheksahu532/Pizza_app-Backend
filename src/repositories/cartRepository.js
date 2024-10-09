34cb1f71174aa78b0fefa266d8037ba9030a2b73
Abhishek Sahu <abhishekkumarsahu532@gmail.com>
2024-10-10 02:01:21 +0530

    Clear cart api added

--- src/repositories/cartRepository.js
+++ src/repositories/cartRepository.js
@@ -1,6 +1,7 @@
 const Cart = require('../schema/cartSchema');
 const BadRequestError = require('../utils/badRequestError');
 const InternalServerError = require('../utils/internalServerError');
+const NotFoundError = require('../utils/notFoundError');
 
 async function createCart(userId){
     try{
@@ -35,7 +36,30 @@ async function getCartByUserId(userId){
 
 }
 
+async function clearCart(userId){
+    try{
+        const cart = await Cart.findOne({
+            user : userId
+        });
+        if(!cart){
+            throw new NotFoundError("Cart");
+        }
+
+        cart.items = [];
+
+        await cart.save();
+
+        console.log(cart);
+
+        return cart;
+
+    } catch(error){
+        throw new InternalServerError();
+    }
+}
+
 module.exports = {
     createCart,
-    getCartByUserId
+    getCartByUserId,
+    clearCart
 }
