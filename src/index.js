d37f02f3b508710f7cfad47c3a971a72c7a2e234
Abhishek Sahu <abhishekkumarsahu532@gmail.com>
2024-10-16 01:02:21 +0530

    cleaned index.js

--- src/index.js
+++ src/index.js
@@ -27,31 +27,8 @@ app.use('/auth', authRouter);
 app.use('/products', productRouter); 
 app.use('/orders', orderRouter); 
 
-app.get('/ping', isLoggedIn, (req,res) =>{
-    console.log(req.body);
-    console.log(req.cookies);
-    return res.json({mesaage: "Pong"});
-});
-
-app.post('/photo', uploader.single('fileName'), async (req,res) =>{
-    console.log(req.file);
-    const result = await cloudinary.uploader.upload(req.file.path);
-    console.log(result);
-    fs.unlink(req.file.path);
-    return res.json({mesaage: "Photo Uploaded"});
-});
 
 app.listen(ServerConfig.PORT, async ()=>{
     await connectDB();
     console.log(`Server Started at ${ServerConfig.PORT}`);
-
-    // const newUser =await User.create({
-    //     firstName: "Abhishek",
-    //     lastName: "kumar",
-    //     email: "kumar@gmail.com",
-    //     mobileNumber: "1231231230",
-    //     password : "123456"
-    // });
-    // console.log("Created a new User");
-    // console.log(newUser);
 });
